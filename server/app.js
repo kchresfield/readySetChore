19afbf58a63daee2baf9d36ecb5eefa0c3b4aeee
kchresfield <kaelyn.chresfield@gmail.com>
2022-04-14 13:34:04 -0500

    working server, connecting server to client and db files

--- server/app.js
+++ server/app.js
@@ -1,7 +1,16 @@
-require ('dotenv').config()
+require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const db = require('../back-end/database.js')
-const client = require('../front-end/client.js')
+const db = require('../back-end/database.js');
+const client = require('../front-end/client.js');
+
+const app = express();
+
+app.use('/', (req, res) => {
+    console.log("Hello World")
+})
+
+
+module.exports = app
